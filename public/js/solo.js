9051d9f5f1f85255add86ebeea839f22a5718c29
Aniruddha Das <aniruddha.das.852@gmail.com>
2021-10-31 03:52:19 +0530

    fixed WPM

--- public/js/solo.js
+++ public/js/solo.js
@@ -11,8 +11,10 @@ const typedValueElement = document.getElementById('typed-value');
 var strted = false;
 var elapsedTime = 0;
 var wpm = 0;
+let timer;
 
 document.getElementById('start').addEventListener('click', () => {
+	strted = false;
 	const quoteIndex = Math.floor(Math.random() * quotes.length);
 	const quote = quotes[quoteIndex];
 	words = quote.split(' ');
@@ -22,16 +24,15 @@ document.getElementById('start').addEventListener('click', () => {
 	quoteElement.children[0].className = 'highlight';
 	messageElement.innerText = '';
 	typedValueElement.value = '';
-	document.getElementById("start").classList.remove("act");
 	document.getElementById("start").classList.add("nodisplay");
-	document.getElementById("typed-value").classList.add("act");
 	document.getElementById("typed-value").classList.remove("nodisplay");
+	document.getElementById("wpm").classList.remove("nodisplay");
 	typedValueElement.focus();
-	setInterval(() => {
+	timer = setInterval(() => {
 		elapsedTime = new Date().getTime() - startTime;
 		wpm = Math.round(((wordIndex) / (elapsedTime / 60000)) * 100) / 100;
 		console.log(wpm);
-		document.getElementById('wpm').innerText = wpm;
+		document.getElementById('wpm').innerText = "WPM: " + wpm;
 	}, 1000);
 });
 
@@ -40,7 +41,8 @@ typedValueElement.addEventListener('input', () => {
 		strted = true;
 		startTime = new Date().getTime();
 	}
-	console.log(wpm);
+	// console.log(wpm);
+	document.getElementById('wpm').innerText = "WPM: " + wpm;
 	const currentWord = words[wordIndex];
 	const typedValue = typedValueElement.value;
 	if (typedValue == currentWord && wordIndex === words.length - 1) {
@@ -50,10 +52,10 @@ typedValueElement.addEventListener('input', () => {
 		const message = `You're WPM is ${wpm}`;
 		messageElement.innerText = message;
 		quoteElement.children[wordIndex].className = "";
-		document.getElementById("typed-value").classList.remove("act");
 		document.getElementById("typed-value").classList.add("nodisplay");
-		document.getElementById("start").classList.add("act");
 		document.getElementById("start").classList.remove("nodisplay");
+		document.getElementById("wpm").classList.add("nodisplay");
+		// clearInterval(timer);
 	} else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
 		typedValueElement.value = '';
 		quoteElement.children[wordIndex].className = '';
@@ -66,5 +68,4 @@ typedValueElement.addEventListener('input', () => {
 		typedValueElement.className = "form-control form-control-lg is-invalid";
 		quoteElement.children[wordIndex].className = 'error';
 	}
-	document.getElementById('wpm').innerText = wpm;
 });
