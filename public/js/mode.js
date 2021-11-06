a7f5daf2b2a8325470f4ad8284dede979c83b8cb
Aniruddha <37500406+OctoplusNinja@users.noreply.github.com>
2021-11-06 21:31:26 +0530

    Update mode.js

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,7 +1,7 @@
 document.getElementById("onClickSolo").onclick = function () {
-	location.href = "/solo";
+	location.href = "/solo.html";
 };
 
 document.getElementById("onClick1v1").onclick = function () {
-	location.href = "/1v1";
+	location.href = "/1v1.html";
 };
